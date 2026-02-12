f858097343084a0c07626fe54ff86f1d4b366b1b
Ajit Singh <ajitsinghds02@gmail.com>
2026-02-12 12:39:08 +0530

    updated main.py

--- main.py
+++ main.py
@@ -260,5 +260,7 @@ def get_categories() -> str:
 
 # --- Start the Server ---
 
+# Start the server
 if __name__ == "__main__":
-    mcp.run(transport='http',host='0.0.0.0',port=8000)
+    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    # mcp.run()
